f224ebb655d91336ec04fe89976d67d9301ef8b1
suresh bethanasamy <suresh.b@WSEINML-GSRZFVH.local>
2020-01-02 15:42:31 +0530

    Mern app host  - react and restapi-service

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const app = express();
 const cors = require('cors');
 const postRoutes = require('./routes/myPost');
 const userRoutes = require('./routes/user');
-const port = process.env.PORT || 4949;
 require('dotenv').config();
 
 // Middleware
@@ -31,6 +30,18 @@ app.use(express.json());
 // Routes Middleware
 app.use('/api/post', postRoutes);
 app.use('/api/user', userRoutes);
+
+// Port assign
+const port = process.env.PORT || 4949;
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+    });
+}
 
 // Listen the app with port
 app.listen(port, () => (console.log(`server running on ${port}.`)));

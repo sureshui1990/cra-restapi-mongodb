c94b1107df9aeeccc34daff6332f6f3088d8c9f1
sureshui1990 <sureshui1990@gmail.com>
2020-02-21 12:13:10 -0600

    SemanticUiReact package implemendation instead of materialUI

--- client/src/containers/login/Login.container.js
+++ client/src/containers/login/Login.container.js
@@ -1,116 +1,115 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Grid, Button, TextField, Container,Card,CardContent,Typography  } from '@material-ui/core';
-import { Login } from '../../utils/auth';
-import { Link } from 'react-router-dom';
-import InputIcon from '@material-ui/icons/Input';
-import CreateIcon from '@material-ui/icons/Create';
-import { connect } from 'react-redux';
-import { LoginUrl } from '../../constants';
-import { LOGIN } from '../../actions/User';
-
+import React, { Component } from "react";
+import axios from "axios";
+import { Container, Grid, Segment, Button, Form,Message,Icon, Input } from "semantic-ui-react";
+import { Login } from "../../utils/auth";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { LoginUrl } from "../../constants";
+import { LOGIN } from "../../actions/User";
 
 class LoginContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-             email: '',
-             password:'',
-             error:false
-        }
-    }
-    
-    handleOnChange = event => {
-        this.setState({ [event.target.name] : event.target.value });
-    }
-    handleLoginSumbit = () => {
-        const { email, password } = this.state;
-        const { login } = this.props;
-        const reqBody = { email,password };
-        axios.post(LoginUrl, reqBody)
-      .then(response => {
-        this.setState({success:response}, ()=>{
-            this.userLogin(response.data["auth-token"]);
-            login(response.data);
-        });
-      },(error) => {
-            this.setState({error:error.response.data});
-      })
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      error: false
     };
-    userLogin = (token) => {
-        Login(token);
-    }
-    render() {
-        const {email,password,error } = this.state;
-        const isSubmitDisable = (email === "") || (password === "");
-        const {formateError,emailError,passwordError,message } = error;
-        
-        return (
-            <React.Fragment>
-                <main className="main">
-                <Container maxWidth="xs">
-                    <Card>
-                        <CardContent>
-                         <Grid container justify="center">
-                            <Grid item xs={12} sm={10}>
-                    <form noValidate autoComplete="off">
-                      <TextField
-                            label="Email" type="text" margin="dense"
-                            size="small" fullWidth
-                            autoFocus={true} name="email"
-                            required={true} value={email}
-                            onChange={this.handleOnChange}
-                            error={emailError}
-                            helperText={emailError && `${message}`}
-                            />
-                        <TextField
-                            label="Password" style={{margin:'1em 0'}}
-                            type="password" margin="dense"
-                            fullWidth name="password" value={password}
-                            onChange={this.handleOnChange}                        
-                            error={passwordError}
-                            helperText={passwordError && `${message}`}
-                            />
-                            <div>
-                            <Button color="primary"
-                            disabled={isSubmitDisable}
-                            fullWidth
-                            variant="contained" onClick={this.handleLoginSumbit}
-                            type="button">
-                                <InputIcon />
-                                Login</Button>
-                        </div>
+  }
 
-                        {formateError && <p style={{color:'#c00'}}>{message}</p>}
-                        <Typography variant='inherit' component="div"
-                         align='center'
-                        >
-                         <Link to="/signup">
-                             <CreateIcon />
-                             Register as a new user</Link>
-                        </Typography>
-                </form>    
-                </Grid>
-                </Grid>
-                </CardContent>
-                    </Card>
-               </Container>
-               </main>
-            </React.Fragment>
-        )
-    }
+  handleOnChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  handleLoginSumbit = () => {
+    const { email, password } = this.state;
+    const { login } = this.props;
+    const reqBody = { email, password };
+    axios.post(LoginUrl, reqBody).then(
+      response => {
+        this.setState({ success: response }, () => {
+          this.userLogin(response.data["auth-token"]);
+          login(response.data);
+        });
+      },
+      error => {
+        this.setState({ error: error.response.data });
+      }
+    );
+  };
+  userLogin = token => {
+    Login(token);
+  };
+  render() {
+    const { email, password, error } = this.state;
+    const isSubmitDisable = email === "" || password === "";
+    const { formateError, message, passwordError } = error;
+
+    return (
+      <React.Fragment>
+        <main className="main">
+          <Container>
+            <Grid columns={3} centered>
+              <Grid.Column>
+                <Segment>
+                  <Form noValidate autoComplete="off">
+                    <Form.Field>
+                    <Input iconPosition="left" placeholder='Email'>
+                      <input
+                        name="email"
+                        value={email}
+                        onChange={this.handleOnChange}
+                      />
+                      <Icon name="at" />
+                      </Input>
+
+                    </Form.Field>
+                    <Form.Field>
+                      <Input iconPosition="left" placeholder='Email'>
+                      <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={this.handleOnChange}
+                      />
+                      <Icon name="keyboard outline" />
+                      </Input>
+                    </Form.Field>
+                    <Form.Field>
+                      <Button
+                        disabled={isSubmitDisable}
+                        onClick={this.handleLoginSumbit}
+                        type="button" size="small"
+                      >
+                        Login
+                      </Button>
+                    </Form.Field>
+
+                    {formateError && <Message color="red">{message}</Message>}
+                    {passwordError && <Message compact negative>{message}</Message>}
+                    <div>
+                      <Link to="/signup">Register as a new user</Link>
+                    </div>
+                  </Form>
+                </Segment>
+              </Grid.Column>
+            </Grid>
+          </Container>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-      counter: state.Counter
-    }
+  return {
+    counter: state.Counter
+  };
 };
-  
-const mapDispatchToProps = dispatch => {
-    return {
-        login: (data) => dispatch(LOGIN(data))
-    }
- };
 
-export default connect(mapStateToProps, mapDispatchToProps) (LoginContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    login: data => dispatch(LOGIN(data))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
